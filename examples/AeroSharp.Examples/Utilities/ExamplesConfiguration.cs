6a0e669720cbc5de428549d20cb848d41b50d41e
Will Baldoumas <45316999+wbaldoumas@users.noreply.github.com>
2022-10-14 13:55:19 -0700

    Fix Aerosharp.Examples and Aerosharp.HelloWorld (#160)
    Co-authored-by: Will <wbaldoumas@wayfair.com>

--- examples/AeroSharp.Examples/Utilities/ExamplesConfiguration.cs
+++ examples/AeroSharp.Examples/Utilities/ExamplesConfiguration.cs
@@ -2,6 +2,6 @@
 {
     internal static class ExamplesConfiguration
     {
-        public const string AerospikeNamespace = "test_namespace";
+        public const string AerospikeNamespace = "test";
     }
 }
