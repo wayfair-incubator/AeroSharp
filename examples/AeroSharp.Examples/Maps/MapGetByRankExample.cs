cdd30e48c5961c082ff6c2109748882c6dd82f93
Peter Minassian <peter.a.minassian@gmail.com>
2023-08-05 17:23:17 -0400

    Allow for example to change the ordering of the map

--- examples/AeroSharp.Examples/Maps/MapGetByRankExample.cs
+++ examples/AeroSharp.Examples/Maps/MapGetByRankExample.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
+using Aerospike.Client;
 using Timer = System.Timers.Timer;
 
 namespace AeroSharp.Examples.Maps;
@@ -20,9 +21,13 @@ public class MapGetByRankExample : IExample
     public MapGetByRankExample(IClientProvider clientProvider)
     {
         var dataContext = new DataContext(ExamplesConfiguration.AerospikeNamespace, SetName);
-
+        var mapConfiguration = new MapConfiguration()
+        {
+            Order = MapOrder.KEY_VALUE_ORDERED
+        };
         _map = MapBuilder.Configure(clientProvider)
             .WithDataContext(dataContext)
+            .WithMapConfiguration(mapConfiguration)
             .Build<string, string>(MapExampleData.RecordKey);
     }
 
