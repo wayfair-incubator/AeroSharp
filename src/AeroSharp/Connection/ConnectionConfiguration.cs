fb978c64ea9da43f3aed6e36891772229174f9e7
Nan Mu <munan.geo@gmail.com>
2024-08-13 11:08:07 -0500

    Add more comments

--- src/AeroSharp/Connection/ConnectionConfiguration.cs
+++ src/AeroSharp/Connection/ConnectionConfiguration.cs
@@ -18,7 +18,11 @@ namespace AeroSharp.Connection
             ConnectionTimeout = TimeSpan.FromSeconds(10);
             AsyncMaxCommands = 500;
             MaxCommandAction = MaxCommandAction.DELAY; // Recommended by Aerospike Enterprise Support (behavior when asyncMaxCommands is exceeded)
-            MaxConnsPerNode = 100; // Leave it as default value from Aerospike
+            // Leave it as default value from Aerospike Client.
+            // Note: Aerospike client has updated their default settings, see the change reasons from Aerospike forum: https://discuss.aerospike.com/t/client-1772-client-configurations-changes-reasons/9699
+            // We will update AsyncMaxCommands to 100 when we bump up Aerospike version so we align with Aerospike settings.
+            // Relationship between asyncMaxConnsPerNode and MaxConnsPerNode can be found from here: https://aerospike.com/apidocs/csharp/html/f_aerospike_client_asyncclientpolicy_asyncmaxconnspernode
+            MaxConnsPerNode = 100;
         }
 
         /// <summary>
