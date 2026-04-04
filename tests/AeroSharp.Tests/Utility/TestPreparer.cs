f716d946b9e2f2b0b7676cc2566055c599d3255b
Peter Minassian <peter.a.minassian@gmail.com>
2026-04-03 20:40:17 -0400

    buffer

--- tests/AeroSharp.Tests/Utility/TestPreparer.cs
+++ tests/AeroSharp.Tests/Utility/TestPreparer.cs
@@ -6,38 +6,12 @@ namespace AeroSharp.Tests.Utility
 {
     public class TestPreparer
     {
-        public class CachedClientProvider : IClientProvider
-        {
-            private readonly IClientProvider _innerProvider;
-            private static ClientWrapper _instance;
-            private static readonly object Lock = new();
-
-            public CachedClientProvider(IClientProvider innerProvider)
-            {
-                _innerProvider = innerProvider;
-            }
-
-            public ClientWrapper GetClient()
-            {
-                if (_instance is not null)
-                {
-                    return _instance;
-                }
-
-                lock (Lock)
-                {
-                    _instance ??= _innerProvider.GetClient();
-                }
-
-                return _instance;
-            }
-        }
-
         private static readonly string[] BootstrapServers = { "127.0.0.1" };
         private const int Port = 3000;
 
         public const string TestSet = "test_set";
         public const string TestNamespace = "test";
+
         public static DataContext TestDataContext => new(TestNamespace, TestSet);
 
         /// <summary>
@@ -50,10 +24,8 @@ namespace AeroSharp.Tests.Utility
                 .Configure()
                 .WithContext(new ConnectionContext(BootstrapServers, Port))
                 .WithoutCredentials()
-                .WithConfiguration(new ConnectionConfiguration { ConnectionTimeout = TimeSpan.FromMilliseconds(100) })
+                .WithConfiguration(new ConnectionConfiguration { ConnectionTimeout = TimeSpan.FromMilliseconds(2000) })
                 .Build();
-
-            var cachedClientProvider = new CachedClientProvider(clientProvider);
 
             SetTruncatorBuilder
                 .Configure(clientProvider)
@@ -61,7 +33,7 @@ namespace AeroSharp.Tests.Utility
                 .Build()
                 .TruncateSet();
 
-            return cachedClientProvider;
+            return clientProvider;
         }
     }
 }
